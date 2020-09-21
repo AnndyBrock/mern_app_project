e4e3e2d65cfb261eeac94221d9c10065835a12d5
atroian <andrii.troian@corezoid.com>
2020-09-21 17:43:58 +0300

    enable fields on main page after logout

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -17,6 +17,10 @@ export const AuthPage = () => {
         clearError()
     },[error, msg, clearError]);
 
+    useEffect(()=> {
+        window.M.updateTextFields()
+    },[]);
+
     const changeHandler = event => {
         setForm({...form, [event.target.name]:event.target.value})
     };
